8e713f4530c71b030ee8230b1489ac930fb306d6
replaceroot <1762458611@qq.com>
2019-06-20 16:11:08 +0800

    初始化项目完成,完成了一个没有样式的简单login登录页面

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- 挖坑 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+
+</style>
