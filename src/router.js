382df0ff4f3cbd9f68df7d4c1d44ce25666913a4
replaceroot <1762458611@qq.com>
2019-06-21 16:58:39 +0800

    完成首页整体结构,下一步完成拦截器实现token值的传递功能

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 // 引入登录组件
 import Login from '@/views/login.vue'
 import Home from '@/views/Home.vue'
+import Welcome from '@/views/Welcome.vue'
 Vue.use(VueRouter)
 
 // 创建路由模块
@@ -17,7 +18,17 @@ var router = new VueRouter({
     {
       name: 'Home',
       path: '/home',
-      component: Home
+      component: Home,
+      // 重定向到welcome页面
+      redirect: { name: 'Welcome' },
+      // 创建子路由
+      children: [
+        {
+          name: 'Welcome',
+          path: 'welcome',
+          component: Welcome
+        }
+      ]
     }
   ]
 })
---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wel">欢迎你使用电商后台管理系统</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  // 生成全局作用域样式
+  .wel {
+    color: red;
+  }
+</style>
